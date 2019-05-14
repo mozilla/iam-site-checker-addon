f6e7245f017e8e0660dbb60b8303c64d567768f9
Mike Cooper <mythmon@gmail.com>
2019-05-14 16:45:34 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Mozilla IAM Site Checker
+
+This is an in-development add-on that puts a small green checkmark in the
+address bar for sites on which it is safe to log in to the Mozilla IAM system
+(aka "your LDAP password"). On other sites there is a hidden page action in
+the menu that shows a red X and a note that it is not safe to use your
+Mozilla IAM credentials on this site.
+
+The list of sites is hard coded into the add-on. The current list is:
+
+* `auth.mozilla.auth0.com`
+* `login.mozilla.com`
+
+# Development
+
+```
+yarn install --frozen-lockfile
+yarn build
+```
+
+A built add-on will be placed in `./web-ext-artifacts/`.
---
+++ background.js
@@ -0,0 +1,79 @@
+const TRUSTED_DOMAINS = new Set([
+  "auth.mozilla.auth0.com",
+  "login.mozilla.com",
+]);
+
+(async () => {
+  for (const tab of await browser.tabs.query({ discarded: false })) {
+    if (isTrustedUrl(tab.url)) {
+      markAsTrusted(tab.id);
+    } else {
+      markAsUntrusted(tab.id);
+    }
+  }
+})();
+
+function isTrustedUrl(urlString) {
+  if (!urlString) {
+    return false;
+  }
+  const parsedUrl = new URL(urlString);
+  let trusted = TRUSTED_DOMAINS.has(parsedUrl.hostname);
+  return trusted;
+}
+
+browser.webNavigation.onBeforeNavigate.addListener(({ frameId, tabId }) => {
+  if (frameId !== 0) {
+    // Not a top level frame, so take no action
+    return;
+  }
+  markAsUnknown(tabId);
+});
+
+browser.webNavigation.onCompleted.addListener(({ frameId, tabId, url }) => {
+  if (frameId !== 0) {
+    // Not a top level frame, so take no action
+    return;
+  }
+  if (isTrustedUrl(url)) {
+    markAsTrusted(tabId);
+  } else {
+    markAsUntrusted(tabId);
+  }
+});
+
+function markAsTrusted(tabId) {
+  browser.pageAction.setIcon({
+    path: "imgs/trusted.png",
+    tabId,
+  });
+  browser.pageAction.setTitle({
+    title: "It is SAFE to use your mozilla credentials on this site",
+    tabId,
+  })
+  browser.pageAction.show(tabId);
+}
+
+function markAsUntrusted(tabId) {
+  browser.pageAction.setIcon({
+    path: "imgs/untrusted.png",
+    tabId,
+  });
+  browser.pageAction.setTitle({
+    title: "It is UNSAFE to use your Mozilla credentials on this site",
+    tabId,
+  })
+  browser.pageAction.hide(tabId);
+}
+
+function markAsUnknown(tabId) {
+  browser.pageAction.setIcon({
+    path: "imgs/unknown.png",
+    tabId,
+  });
+  browser.pageAction.setTitle({
+    title: "It is UNKNOWN if it is safe to use Mozilla credentials on this site",
+    tabId,
+  })
+  browser.pageAction.hide(tabId);
+}
